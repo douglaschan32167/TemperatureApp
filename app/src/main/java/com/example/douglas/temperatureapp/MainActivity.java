88ce32b179d7c6fa9b9a23554eeacfb2accb485e
douglaschan32167 <douglaschan32167@gmail.com>
2016-10-06 10:06:58 -0700

    basic ui with functional C/F toggle button and table showing temperatures with days

--- app/src/main/java/com/example/douglas/temperatureapp/MainActivity.java
+++ app/src/main/java/com/example/douglas/temperatureapp/MainActivity.java
@@ -6,12 +6,20 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private Button toggleDegreesButton;
     private SensorManager sensorManager;
     private Sensor ambientTempSensor;
-    private TextView testView, ambientTempView;
+    private String degreeType;
+    private TemperatureGenerator tempGen;
+    private TextView ambientTempView, testView, degreesTypeView;
+    private TextView mondayDegrees, tuesdayDegrees, wednesdayDegrees, thursdayDegrees, fridayDegrees;
 
     private float ambientTemp;
 
@@ -20,29 +28,70 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
 
 
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        ambientTempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE);
+        this.sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        this.ambientTempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE);
+        this.degreeType = "Celsius";
 
         setContentView(R.layout.activity_main);
-        TemperatureGenerator tempGen = new TemperatureGenerator();
-        testView = (TextView)findViewById(R.id.testTextView);
+        this.tempGen = new TemperatureGenerator();
+        this.toggleDegreesButton = (Button)findViewById(R.id.degreesToggleButton);
+        this.testView = (TextView)findViewById(R.id.testTextView);
+        this.degreesTypeView = (TextView) findViewById(R.id.degreesType);
+        this.mondayDegrees = (TextView) findViewById(R.id.mondayDegrees);
+        this.tuesdayDegrees = (TextView) findViewById(R.id.tuesdayDegrees);
+        this.wednesdayDegrees = (TextView) findViewById(R.id.wednesdayDegrees);
+        this.thursdayDegrees = (TextView) findViewById(R.id.thursdayDegrees);
+        this.fridayDegrees = (TextView) findViewById(R.id.fridayDegrees);
+
+
         if (ambientTempSensor == null) {
-            testView.setText("There is no ambient temp sensor");
+            this.testView.setText("There is no ambient temp sensor");
         } else {
-            testView.setText("The temps are " + tempGen.getTemperaturesC().toString() + "which in Fahr is " + tempGen.getTemperaturesF().toString());
+            this.testView.setText("The temps are " + this.tempGen.getTemperaturesC().toString() + "which in Fahr is " + this.tempGen.getTemperaturesF().toString());
         }
-        ambientTempView = (TextView)findViewById(R.id.ambientTemperatureView);
+        showTemperatures();
+
+
+        this.ambientTempView = (TextView)findViewById(R.id.ambientTemperatureView);
 
     }
 
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, ambientTempSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        this.sensorManager.registerListener(this, this.ambientTempSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(this);
+        this.sensorManager.unregisterListener(this);
+    }
+
+    private void showTemperatures(){
+        List<Float> temps;
+        if (this.degreeType.equals("Celsius")) {
+            temps = this.tempGen.getTemperaturesC();
+            this.toggleDegreesButton.setText("Show in Fahrenheit");
+        } else {
+            temps = this.tempGen.getTemperaturesF();
+            this.toggleDegreesButton.setText("Show in Celsius");
+        }
+
+
+        this.mondayDegrees.setText(String.valueOf(temps.get(0)));
+        this.tuesdayDegrees.setText(String.valueOf(temps.get(1)));
+        this.wednesdayDegrees.setText(String.valueOf(temps.get(2)));
+        this.thursdayDegrees.setText(String.valueOf(temps.get(3)));
+        this.fridayDegrees.setText(String.valueOf(temps.get(4)));
+        this.degreesTypeView.setText("Degrees " + this.degreeType);
+    }
+
+    public void toggleTemperatureTypes(View view) {
+        if (this.degreeType.equals("Celsius")) {
+            this.degreeType = "Fahrenheit";
+        } else {
+            this.degreeType = "Celsius";
+        }
+        showTemperatures();
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
